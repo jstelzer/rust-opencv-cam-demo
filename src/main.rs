0d63458817f93a97305288d661d86362f747d904
Jason Stelzer <mental@neverlight.com>
2021-02-04 19:40:49 -0500

    Camera works
    Got greyscale working too.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate opencv;
+
+use opencv::{core, highgui, prelude::*, videoio, imgproc};
+
+fn run() -> opencv::Result<()> {
+    let window = "video capture";
+    highgui::named_window(window, 1)?;
+    let mut cam = videoio::VideoCapture::new(1, videoio::CAP_ANY)?; // 0 is the default camera
+    let opened = videoio::VideoCapture::is_opened(&cam)?;
+    if !opened {
+        panic!("Unable to open default camera!");
+    }
+    loop {
+        let mut frame = core::Mat::default()?;
+        cam.read(&mut frame)?;
+        if frame.size()?.width > 0 {
+            let mut gray = core::Mat::default()?;
+            imgproc::cvt_color(
+                &frame,
+                &mut gray,
+                imgproc::COLOR_BGR2GRAY,
+                0
+            )?;
+            highgui::imshow(window, &mut gray)?;
+        }
+        let key = highgui::wait_key(10)?;
+        if key > 0 && key != 255 {
+            break;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap()
 }
