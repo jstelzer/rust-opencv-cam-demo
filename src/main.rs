9ae0c42bcead50e16835223ca0e08e57276992c2
Jason Stelzer <mental@neverlight.com>
2021-02-05 14:52:32 -0500

    Added controls
    Getting usable and not a giant pile of state vars.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,17 @@ enum KeyCodes {
     Space = 32,
     LowerC = 99,
     LowerZ = 122,
+    //T1
     Plus = 43,
-    Underscore = 95,
     Equals = 61,
+    //T2
+    Underscore = 95,
     Minus = 45,
+}
+
+struct CammyOpts {
+    threshold_1: f64,
+    threshold_2: f64,
 }
 
 fn invert_frame(frame: &mut core::Mat) -> core::Mat {
@@ -20,10 +27,10 @@ fn invert_frame(frame: &mut core::Mat) -> core::Mat {
     // make the masking layer empty so the whole thing is inverted
     let no_mask = core::Mat::default().unwrap();
     core::bitwise_not(frame, &mut inverted, &no_mask).unwrap();
-    return inverted;
+    inverted
 }
 
-fn canny_frame(frame: &mut core::Mat) -> core::Mat {
+fn canny_frame(frame: &mut core::Mat, edge_opts: &CammyOpts) -> core::Mat {
     // blur it so that the edges are not excessive
     let mut blur = core::Mat::default().unwrap();
     imgproc::gaussian_blur(
@@ -36,20 +43,33 @@ fn canny_frame(frame: &mut core::Mat) -> core::Mat {
     )
     .unwrap();
     let mut edges = core::Mat::default().unwrap();
-    imgproc::canny(&blur, &mut edges, 30.0, 40.0, 3, true).unwrap();
-    return edges;
+    imgproc::canny(
+        &blur,
+        &mut edges,
+        edge_opts.threshold_1,
+        edge_opts.threshold_2,
+        3,
+        true,
+    )
+    .unwrap();
+    edges
 }
 
 fn greyscale_frame(frame: &mut core::Mat) -> core::Mat {
     let mut gray = core::Mat::default().unwrap();
     imgproc::cvt_color(&frame.clone(), &mut gray, imgproc::COLOR_BGR2GRAY, 0).unwrap();
-    return gray;
+    gray
 }
+
 fn run() -> opencv::Result<()> {
     let window = "Silly image transform";
     highgui::named_window(window, 1)?;
     let mut cam = videoio::VideoCapture::new(1, videoio::CAP_ANY)?; // 0 is the default camera
     let opened = videoio::VideoCapture::is_opened(&cam)?;
+    let mut edge_thresholds = CammyOpts {
+        threshold_1: 30.0,
+        threshold_2: 40.0,
+    };
     if !opened {
         panic!("Unable to open default camera!");
     }
@@ -63,9 +83,8 @@ fn run() -> opencv::Result<()> {
             if greyscale_flag {
                 frame = greyscale_frame(&mut frame);
             }
-            // once we have a frame we grayscale
             if canny_flag {
-                frame = canny_frame(&mut frame);
+                frame = canny_frame(&mut frame, &edge_thresholds);
             }
             if invert_flag {
                 frame = invert_frame(&mut frame);
@@ -74,22 +93,31 @@ fn run() -> opencv::Result<()> {
             highgui::imshow(window, &mut frame)?;
         }
         let key = highgui::wait_key(10)?;
-        //TODO: Wire in keyboard controls to adjust edge detection thresholds as well as toggle everything else.
-        // keys come back as the decimal value of the ascii(7) code set.
         if key > -1 {
             // -1 is when nothing is pressed.
             match FromPrimitive::from_i32(key) {
                 Some(KeyCodes::Esc) => break, //esc key
                 Some(KeyCodes::LowerC) => {
-                    println!("Got key {}", key);
                     invert_flag = !invert_flag;
-                }
+                },
                 Some(KeyCodes::Space) => {
                     canny_flag = !canny_flag;
-                }
+                },
                 Some(KeyCodes::LowerZ) => {
                     greyscale_flag = !greyscale_flag;
-                }
+                },
+                Some(KeyCodes::Plus) => {
+                    edge_thresholds.threshold_1 = edge_thresholds.threshold_1 + 1.0;
+                },
+                Some(KeyCodes::Equals) => {
+                    edge_thresholds.threshold_1 = edge_thresholds.threshold_1 - 1.0;
+                },
+                Some(KeyCodes::Underscore) => {
+                    edge_thresholds.threshold_2 = edge_thresholds.threshold_2 + 1.0;
+                },
+                Some(KeyCodes::Minus) => {
+                    edge_thresholds.threshold_2 = edge_thresholds.threshold_2 - 1.0;
+                },
                 _ => println!("Unmapped key {}", key),
             }
         }
