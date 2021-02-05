30d1117d93eead538ebfe0b1c9e6dbab021cb898
Jason Stelzer <mental@neverlight.com>
2021-02-04 20:32:05 -0500

    more progress

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 #export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/Toolchains/XcodeDefault.xctoolchain/usr/lib/"
 export DYLD_FALLBACK_LIBRARY_PATH=/Library/Developer/CommandLineTools/usr/lib/
-export CV_BASE=/usr/local/Cellar/opencv/4.5.1_2
+#export CV_BASE=/usr/local/Cellar/opencv/4.5.1_2
+export CV_BASE=/usr/local/opt/opencv
 export OpenCV_DIR=${CV_BASE}
 #export OPENCV_LINK_LIBS=${CV_BASE}/lib
 export OPENCV_LINK_PATHS=${CV_BASE}/lib 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate opencv;
 
-use opencv::{core, highgui, prelude::*, videoio, imgproc};
+use opencv::{core, highgui, imgproc, prelude::*, videoio};
+
 
 fn run() -> opencv::Result<()> {
     let window = "video capture";
@@ -15,13 +16,15 @@ fn run() -> opencv::Result<()> {
         cam.read(&mut frame)?;
         if frame.size()?.width > 0 {
             let mut gray = core::Mat::default()?;
-            imgproc::cvt_color(
-                &frame,
-                &mut gray,
-                imgproc::COLOR_BGR2GRAY,
-                0
-            )?;
-            highgui::imshow(window, &mut gray)?;
+            imgproc::cvt_color(&frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+            let mut edges = core::Mat::default()?;
+            imgproc::canny(&gray, &mut edges, 70.0, 45.0, 3, true)?;
+
+            let mut inverted = core::Mat::default()?;
+            let no_mask = core::Mat::default()?;
+            core::bitwise_not(&edges, &mut inverted, &no_mask)?;
+//            imgproc::gaussian_blur(src: &dyn core::ToInputArray, dst: &mut dyn core::ToOutputArray, ksize: core::Size, sigma_x: f64, sigma_y: f64, border_type: i32)
+            highgui::imshow(window, &mut inverted)?;
         }
         let key = highgui::wait_key(10)?;
         if key > 0 && key != 255 {
