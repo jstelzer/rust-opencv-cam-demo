f4c61916c24e063868077afa9a407aa3453c33c2
Jason Stelzer <mental@neverlight.com>
2021-02-14 11:37:40 -0500

    More cleanups
    Bare minimum settings working.

--- env.sh
+++ env.sh
@@ -1,5 +1,2 @@
-# Not quite... but got me on track.
-#export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/Toolchains/XcodeDefault.xctoolchain/usr/lib/"
-
-export DYLD_FALLBACK_LIBRARY_PATH=/Library/Developer/CommandLineTools/usr/lib/
+export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/usr/lib"
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${OPENCV_LINK_LIBS}/pkgconfig"
