3baede1d65204d42f43806c8118f18516a30faa7
Jason Stelzer <jason.stelzer@penn-interactive.com>
2021-07-02 14:51:15 -0400

    Osx build issues.

--- env.sh
+++ env.sh
@@ -1,2 +1,5 @@
-export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/usr/lib"
+export OPENCV_LINK_LIBS=/usr/local/opt/opencv/lib
+# They keep fiddling with where this lives.
+#export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/usr/lib"
+export DYLD_FALLBACK_LIBRARY_PATH=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${OPENCV_LINK_LIBS}/pkgconfig"
