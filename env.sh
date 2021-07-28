4f8995e61d8d257c07df8397e724756aebbe7f14
Jason Stelzer <jason.stelzer@penn-interactive.com>
2021-07-28 17:43:53 -0400

    Updated to new version of rust library.

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-export OPENCV_LINK_LIBS=/usr/local/opt/opencv/lib
+#export OPENCV_LINK_LIBS=/usr/local/opt/opencv@4/lib
 # They keep fiddling with where this lives.
 #export DYLD_FALLBACK_LIBRARY_PATH="$(xcode-select --print-path)/usr/lib"
-export DYLD_FALLBACK_LIBRARY_PATH=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/
+export DYLD_FALLBACK_LIBRARY_PATH=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/:/usr/local/lib
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${OPENCV_LINK_LIBS}/pkgconfig"
